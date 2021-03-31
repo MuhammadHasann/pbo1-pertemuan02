05d9c087b27dfa8e2f0b7ea29a9b77d88967321f
MuhammadHasann <muhammadhas772@gmail.com>
2021-03-31 19:26:21 +0700

    Membuat input menggunakan Scanner

--- InputOutput.java
+++ InputOutput.java
@@ -1,4 +1,27 @@
+import java.util.Scanner;
+
 public class InputOutput{
- public static void main(String {} args){
+ public static void main(String [] args){
+  Scanner inputan = new Scanner(System.in);
+  String nama;
+  int jumlahSks;
+  double IPK;
+
+  System.out.print("Masukkan nama anda: ");
+  nama = inputan.nextLine();
+
+  System.out.print("Masukkan jumlah SKS yang sudah ditempuh: ");
+  jumlahSks = inputan.nextInt();
+
+  System.out.print("Masukkan IPK sementara: ");
+  IPK = inputan.nextDouble();
+  
+  System.out.println("Hai, "+nama);
+  System.out.println("sks yang sudah anda tempuh : "+jumlahSks);
+  System.out.println("Nilai : "+IPK);
+
+  double jumlah = IPK * jumlahSks;
+  System.out.println("Total Nilai = "+jumlah);
+  
  }
 }
